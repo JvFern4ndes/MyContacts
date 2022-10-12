4f5a7fef3d7e7168cdb4762797b0519ca1ec388e
João Victor <jvfernandes.contato@gmail.com>
2022-10-12 13:04:30 -0300

    Criando as rotas de show e delete

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,10 +1,17 @@
 const { v4 } = require('uuid');
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: 'Mateus',
     email: 'mateus@mail.com',
+    phone: '123123123123',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Jose',
+    email: 'jose@mail.com',
     phone: '123123123123',
     category_id: v4(),
   },
@@ -14,6 +21,19 @@ class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
     });
   }
 }
