7699b81cbc39c65452be87a82e28df07f43297c2
João Victor <jvfernandes.contato@gmail.com>
2022-10-26 10:18:17 -0300

    Criando a rota Update de Categories

--- src/app/repositories/CategoriesRepository.js
+++ src/app/repositories/CategoriesRepository.js
@@ -20,6 +20,18 @@ class CategoriesRepository {
     return row;
   }
 
+  async update(id, {
+    name,
+  }) {
+    const [row] = await db.query(`
+      UPDATE categories
+      SET name = $1
+      WHERE id = $2
+      RETURNING *
+    `, [name, id]);
+    return row;
+  }
+
   async delete(id) {
     const deleteOp = await db.query('DELETE FROM categories WHERE id = $1', [id]);
     return deleteOp;
